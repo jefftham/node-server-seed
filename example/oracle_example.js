819af622025e77dd7c72631e1d2822b68f5c8ef7
Jeff Tham <jeff.tham@live.com>
2017-08-26 13:44:55 -0400

    example of oracle.js

---
+++ example/oracle_example.js
@@ -0,0 +1,33 @@
+const oracle = require('./../server/util/db/oracle');
+//const config = require('./../server/config'); // load config
+
+// oracle.js already read this config.
+// oracle.setConfig() is used if you did not load the config in  process.env
+let dbConfig = {
+    user: process.env.user,
+    password: process.env.password,
+    connectString: process.env.connectString
+};
+
+oracle.setConfig(dbConfig);
+
+let sqlCommand = `SELECT sysdate FROM dual`;
+
+// let sqlCommand = `SELECT column1, column2 FROM FOO.BAR WHERE id= ;0, name = ;1 `;
+// let param = ['123', 'jeff']
+
+// promise
+oracle.query(sqlCommand, [])
+    .then((results) => {
+        console.log(results.length);
+        console.log(results);
+    }).catch((err) => {
+        console.error(err);
+    });
+
+
+// callback
+oracle.query(sqlCommand, [], (results) => {
+    console.log(results.length);
+    console.log(results);
+})
